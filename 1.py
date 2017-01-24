1e95bff801814c825fc11cec78d2f6e8081f4cd2
Tomas Giedraitis <tomas.g@osos.lt>
2017-01-24 13:40:58 +0200

    sol to 1st ex.

---
+++ 1.py
@@ -0,0 +1,20 @@
+
+
+debug = True
+
+def solution(maximum):
+
+    multiples = []
+
+    for i in range(1, maximum):
+        if not i%3 or not i%5:
+            multiples.append(i)
+
+    return sum(multiples)
+
+max_limit = 1000
+
+result = solution(max_limit)
+
+if debug:
+    print("result", result)
