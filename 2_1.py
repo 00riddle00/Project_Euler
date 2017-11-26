4a27d29db5c78aac714af6d12ad4372933605fec
tomasg <tomas.giedraitis@metasite.net>
2017-11-26 18:06:16 +0200

    add second version of 2nd program

--- 2_1.py
+++ 2_1.py
@@ -1,11 +1,10 @@
 # algorithm complexity:
 
-def sum(prev = None, current = None, sum_current = 0, max_val = None):
+def fibonacci_sum(prev = None, current = None, sum_current = 0, max_val = None):
 
     if not current:
         current = 1
         prev = current
-        # sum_current = 0
 
     if not current%2:
         sum_current += current
@@ -13,9 +12,11 @@ def sum(prev = None, current = None, sum_current = 0, max_val = None):
     next = current + prev
 
     if next < max_val:
-        sum_current = sum(current, next, sum_current, max_val)
+        sum_current = fibonacci_sum(current, next, sum_current, max_val)
 
     return sum_current
 
-result = sum(max_val=4000000)
+
+result = fibonacci_sum(max_val=4000000)
 print("result", result)
+
